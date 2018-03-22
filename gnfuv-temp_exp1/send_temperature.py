1b1f3c684efd699acb34ecd3e91558c04318ef55
NanaBeHart <natascha-harth@live.de>
2018-03-22 14:00:31 +0000

    experiment1 changes

--- gnfuv-temp_exp1/send_temperature.py
+++ gnfuv-temp_exp1/send_temperature.py
@@ -7,7 +7,9 @@ import Adafruit_DHT
 
 
 KAFKA = os.getenv('KAFKA', '192.168.2.250:9092')
-DELTA = float(os.getenv('DELTA', 3))
+DELTA = float(os.getenv('DELTA', 1))
+
+exp=1
 
 gpio = 23
 sensor = Adafruit_DHT.DHT11
@@ -15,10 +17,18 @@ sensor = Adafruit_DHT.DHT11
 def getTempAndHumidity():
     return Adafruit_DHT.read_retry(sensor, gpio)
 
+def savetext(message):
+    str_filename = str(socket.gethostname())+'_'+str(exp)+'.csv'
+    f=open(str_filename,'ab')
+    row= str(message)
+    numpy.savetxt(f, [row], fmt='%s')
+    f.close()
+
 def send():
     try:
        humidity, temperature = getTempAndHumidity()
-       message = {'device': socket.gethostname(), 'temperature': temperature, 'humidity': humidity}
+       message = {'device': socket.gethostname(), 'temperature': temperature, 'humidity': humidity, 'experiment': exp}
+       savetext(message)
        print 'sending', message
        producer = KafkaProducer(bootstrap_servers=KAFKA, value_serializer=lambda v: json.dumps(v).encode('utf-8'))
        producer.send('sensor-reading', message)
@@ -31,4 +41,4 @@ try:
         send()
         time.sleep(DELTA)
 except KeyboardInterrupt:
-    pass
+pass
