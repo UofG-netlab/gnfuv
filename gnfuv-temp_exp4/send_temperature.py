3588ec6b3b3663d0dd2a6a882be9ecd8be3bf3a9
NanaBeHart <natascha-harth@live.de>
2018-03-22 14:22:04 +0000

    Experiment 4 changes

--- gnfuv-temp_exp4/send_temperature.py
+++ gnfuv-temp_exp4/send_temperature.py
@@ -4,10 +4,22 @@ import time
 import json
 import socket
 import Adafruit_DHT
+import pandas as pd
+import numpy
+import statsmodels.formula.api as sm
+import collections
 
 
 KAFKA = os.getenv('KAFKA', '192.168.2.250:9092')
-DELTA = float(os.getenv('DELTA', 3))
+DELTA = float(os.getenv('DELTA', 1))
+exp= float(os.getenv('EXP', 1))
+
+#variables needed
+windowsize=600
+sliding_window_values = collections.deque(maxlen=windowsize)
+parameters_leader=0
+threshold = 0.4
+send='false'
 
 gpio = 23
 sensor = Adafruit_DHT.DHT11
@@ -15,14 +27,57 @@ sensor = Adafruit_DHT.DHT11
 def getTempAndHumidity():
     return Adafruit_DHT.read_retry(sensor, gpio)
 
+def runmodel(sliding_window,values):
+    #put sliding window in dataframe
+    data = list(sliding_window)
+    window_data=pd.DataFrame(data)
+    window_data.columns= ['humidity','temperature']
+    query='temperature ~ humidity'
+    #model fitting
+    result = sm.ols(formula=query, data=window_data).fit()
+    #model parameters
+    param_sensor=list(result.params)
+    localpred_sens=param_sensor[0]+values[0]*param_sensor[1]
+    actual_value=values[1]
+    prediction_leader= parameters_leader[0]+values[0]*parameters_leader[1]
+    
+    local_error=abs(localpred_sens-prediction_leader)
+    return local_error,param_sensor
+
+def savetext(message):
+    str_filename = str(socket.gethostname())+'_'+str(exp)+'.csv'
+    f=open(str_filename,'ab')
+    row= str(message)
+    numpy.savetxt(f, [row], fmt='%s')
+    f.close()
+
 def send():
     try:
        humidity, temperature = getTempAndHumidity()
-       message = {'device': socket.gethostname(), 'temperature': temperature, 'humidity': humidity}
-       print 'sending', message
-       producer = KafkaProducer(bootstrap_servers=KAFKA, value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-       producer.send('sensor-reading', message)
-       producer.flush()
+       
+       values=[humidity,temperature]
+       #appending window
+       sliding_window_values.append(values)
+       
+       if len(sliding_window_values_sens)>= windowsize:
+           diff, parameters= runmodel(sliding_window_values,values)
+           if diff>=threshold:
+               send='true'
+               message = {'device': socket.gethostname(), 'temperature': temperature, 'humidity': humidity, 'parameters': parameters, 'experiment': exp, 'send_status': send}
+               savetext(message)
+               print 'sending', message
+               producer = KafkaProducer(bootstrap_servers=KAFKA, value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+               producer.send('sensor-reading', message)
+               producer.flush()
+           else:
+               send='false'
+               message = {'device': socket.gethostname(), 'temperature': temperature, 'humidity': humidity, 'parameters': parameters, 'experiment': exp, 'send_status': send}
+               savetext(message)
+               print 'sending', message
+               producer = KafkaProducer(bootstrap_servers=KAFKA, value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+               producer.send('sensor-reading', message)
+               producer.flush()
+           
     except Exception as e:
        print 'where r u kafka?', e
 
@@ -31,4 +86,4 @@ try:
         send()
         time.sleep(DELTA)
 except KeyboardInterrupt:
-    pass
+pass
