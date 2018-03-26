86385a12da077f61aef11ffede3226e488f4f486
NanaBeHart <natascha-harth@live.de>
2018-03-26 16:52:35 +0100

    exp 6

--- gnfuv-temp-exp6/send_temperature.py
+++ gnfuv-temp-exp6/send_temperature.py
@@ -30,16 +30,19 @@ gpio = 23
 sensor = Adafruit_DHT.DHT11
 
 def r2score_calc(y_true,y_pred):
-    numerator = numpy.sum((y_true - y_pred) ** 2)
-    denominator = numpy.sum(((y_true - numpy.average(y_true)) ** 2))
-    output_scores = 1 - (numerator /denominator)
-    return output_scores
+    numerator = numpy.nan_to_num(numpy.sum((y_true - y_pred) ** 2))
+    denominator = numpy.nan_to_num(numpy.sum(((y_true - numpy.average(y_true)) ** 2)))
+    if denominator==0:
+        return 0
+    else:
+        output_scores = 1 - (numerator /denominator)
+        return output_scores
     
 
 def getTempAndHumidity():
     #import random
-    #hum= random.randint(1,101)
-    #temp= random.randint(1,200) 
+    #hum= random.uniform(10,101)
+    #temp= random.uniform(25,30) 
     #return hum,temp
     return Adafruit_DHT.read_retry(sensor, gpio)
 
@@ -65,6 +68,8 @@ def runmodel(sliding_window,values):
         for datapoint in range(len(window_data['temperature'])):
             pred=parameters_prev[0]+window_data.loc[datapoint,'humidity']*parameters_prev[1] 
             y_pred.append(pred)
+        from sklearn.metrics import r2_score
+        print(r2_score(window_data['temperature'],y_pred))
         r2_old=r2score_calc(window_data['temperature'],y_pred)
         diff=abs(r2_prev[-1]-r2_old)
         r2_old_push.append(r2_old)
@@ -109,7 +114,7 @@ def send():
            else:
                send='false'
                message = {'time': time.time(),'device': socket.gethostname(), 'temperature': temperature, 'humidity': humidity, 'parameters': parameters_model[-1], 'Windowsize': WINDOWSIZE, 'Threshold': THRESHOLD , 'R2_current': r2_prev[-1],'R2_old': r2_old_push[-1] ,'difference': difference[-1], 'experiment': EXPERIMENT, 'send_status': send}
-               #print(message)
+               print(message)
                savetext(message)
                print 'sending', message
                producer = KafkaProducer(bootstrap_servers=KAFKA, value_serializer=lambda v: json.dumps(v).encode('utf-8'))
